1b77aa8719bf2154e07a1098bb3d48e8cc6088de
Krzysztof Kaszanek <krzysztof.kaszanek@kiwee.eu>
2019-01-12 12:13:48 +0100

    Move secret key to secret_settings

--- MeetAndClimb/settings.py
+++ MeetAndClimb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from MeetAndClimb.secret_settings import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3%r8-3xxrq50co)$v$44#$-ne=orjgnw1q7nf!q+vl70q24-7w'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
