05187d89feaa6e73887a8946a1ddb27cb1862ec7
kasztof <kaszanek.krzysztof@gmail.com>
2019-01-17 23:28:53 +0100

    Modify SignUpform, change language to polish

--- WebApp/forms.py
+++ WebApp/forms.py
@@ -4,12 +4,25 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=32, help_text='First name')
-    last_name = forms.CharField(max_length=32, help_text='Last name')
-    email = forms.EmailField(max_length=64, help_text='Enter a valid email address')
+    first_name = forms.CharField(max_length=32, label='Imię')
+    last_name = forms.CharField(max_length=32, label='Nazwisko')
+    email = forms.EmailField(max_length=64, label='Email')
 
-    class Meta(UserCreationForm.Meta):
+    class Meta:
         model = User
-        # I've tried both of these 'fields' declaration, result is the same
-        # fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
-        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name')
+        fields = ('username', 'password1', 'password2', 'first_name', 'last_name', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        if commit:
+            user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views import generic
 from WebApp.forms import SignUpForm
-
-
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
 
 
 def create_new_user(request):
