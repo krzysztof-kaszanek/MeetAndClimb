f9ac4d43bad18d2aa0b2d455065001de667e35e1
kasztof <kaszanek.krzysztof@gmail.com>
2019-01-19 21:17:32 +0100

    dodano zgloszenia i zapisy do navbara

--- WebApp/context_processor.py
+++ WebApp/context_processor.py
@@ -1,4 +1,4 @@
-from WebApp.models import Wiadomosc, Wspinacz
+from WebApp.models import Wiadomosc, Wspinacz, Instruktor
 
 
 def my_context_processor(request):
@@ -6,6 +6,11 @@ def my_context_processor(request):
         wspinacz = Wspinacz.objects.get(user=request.user)
         wiadomosci = Wiadomosc.objects.all().filter(odbiorca=wspinacz, przeczytana=False)
         liczba_wiadomosci = len(wiadomosci)
-        return {'liczba_wiadomosci': liczba_wiadomosci}
+        instruktor = Instruktor.objects.filter(wspinacz_ptr=wspinacz)
+        if instruktor.count() > 0:
+            uzytkownik_instruktorem = True
+        else:
+            uzytkownik_instruktorem = False
+        return {'liczba_wiadomosci': liczba_wiadomosci, 'uzytkownik_instruktorem': uzytkownik_instruktorem}
     else:
-        return {'liczba_wiadomosci': 0}
+        return {'liczba_wiadomosci': 0, 'uzytkownik_instruktorem': False}
